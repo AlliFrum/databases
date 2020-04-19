49bc4b082c105c01785e5569289abf8303121d02
Alyona <44830907+AlliFrum@users.noreply.github.com>
2020-04-19 19:02:53 +0300

    Update populate.sql

--- populate.sql
+++ populate.sql
@@ -1,20 +1,20 @@
-INSERT INTO UNITS(unit_name, attack, defence, speed, gold) VALUES ('Archangel', 30, 30, 18, 5000);
-INSERT INTO UNITS(unit_name, attack, defence, speed, gold) VALUES ('GoldDragon', 27, 27, 16, 4000);
-INSERT INTO UNITS(unit_name, attack, defence, speed, gold) VALUES ('Titan', 24, 24, 11, 5000);
-INSERT INTO UNITS(unit_name, attack, defence, speed, gold) VALUES ('ArchDevil', 26, 28, 17, 4500);
-INSERT INTO UNITS(unit_name, attack, defence, speed, gold) VALUES ('BlackDragon', 25, 25, 15, 4000);
+INSERT INTO UNITS(unit_name, attack, defence, speed, gold) VALUES ('Angel', 20, 20, 12, 3000);
+INSERT INTO UNITS(unit_name, attack, defence, speed, gold) VALUES ('Cyclops', 15, 12, 6, 750);
+INSERT INTO UNITS(unit_name, attack, defence, speed, gold) VALUES ('Gorgon', 10, 14, 5, 525);
+INSERT INTO UNITS(unit_name, attack, defence, speed, gold) VALUES ('Medusa', 9, 9, 5, 300);
+INSERT INTO UNITS(unit_name, attack, defence, speed, gold) VALUES ('StoneGolem', 7, 10, 3, 150);
 
 
-INSERT INTO CASTLE (castle, unit_name) VALUES ('Castle', 'Archangel');
-INSERT INTO CASTLE (castle, unit_name) VALUES ('Rampart', 'GoldDragon');
-INSERT INTO CASTLE (castle, unit_name) VALUES ('Tower', 'Titan');
-INSERT INTO CASTLE (castle, unit_name) VALUES ('Inferno', 'ArchDevil');
-INSERT INTO CASTLE (castle, unit_name) VALUES ('Dungeon', 'BlackDragon');
+INSERT INTO CASTLE (castle, unit_name) VALUES ('Castle', 'Angel');
+INSERT INTO CASTLE (castle, unit_name) VALUES ('Stronghold', 'Cyclops');
+INSERT INTO CASTLE (castle, unit_name) VALUES ('Tower', 'StoneGolem');
+INSERT INTO CASTLE (castle, unit_name) VALUES ('Fortress', 'Gorgon');
+INSERT INTO CASTLE (castle, unit_name) VALUES ('Dungeon', 'Medusa');
 
 
-INSERT INTO UNIT_LEVELS (unit_level, unit_name) VALUES ('7+', 'Archangel');
-INSERT INTO UNIT_LEVELS (unit_level, unit_name) VALUES ('7+', 'GoldDragon');
-INSERT INTO UNIT_LEVELS (unit_level, unit_name) VALUES ('7+', 'Titan');
-INSERT INTO UNIT_LEVELS (unit_level, unit_name) VALUES ('7+', 'ArchDevil');
-INSERT INTO UNIT_LEVELS (unit_level, unit_name) VALUES ('7+', 'BlackDragon');
+INSERT INTO UNIT_LEVELS (unit_level, unit_name) VALUES ('7', 'Angel');
+INSERT INTO UNIT_LEVELS (unit_level, unit_name) VALUES ('6', 'Cyclops');
+INSERT INTO UNIT_LEVELS (unit_level, unit_name) VALUES ('5', 'Gorgon');
+INSERT INTO UNIT_LEVELS (unit_level, unit_name) VALUES ('4', 'Medusa');
+INSERT INTO UNIT_LEVELS (unit_level, unit_name) VALUES ('3', 'StoneGolem');
 
